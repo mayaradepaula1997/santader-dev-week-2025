e45e46f7b6269ca5ee4f6641b8b4832819e52bdb
Mayara <mayaradepaula1997>
2025-09-23 22:55:19 -0300

    first commit

---
+++ src/main/java/com/dio/santader_dev_week_2025/SantaderDevWeek2025Application.java
@@ -0,0 +1,13 @@
+package com.dio.santader_dev_week_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaderDevWeek2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaderDevWeek2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/santader_dev_week_2025/SantaderDevWeek2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santader_dev_week_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaderDevWeek2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
